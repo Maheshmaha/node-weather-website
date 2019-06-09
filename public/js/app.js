505eb9521dcff3bd288e4b006345fb593a34536f
Mahalingappa <MMahalingapp@ad.harman.com>
2019-06-09 12:20:50 +0530

    removed unnecesary console.log statements

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,9 @@
-console.log("Client side javscript file is loaded");
 
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    })
-})
-
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
